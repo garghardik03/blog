c123a53f928a032ede9c6efa1f0ad0325c4bd65e
Hardik <hgarg1_be21@thapar.edu>
2023-07-10 17:18:29 +0530

    website updated

--- app.py
+++ app.py
@@ -1,60 +1,198 @@
-from flask import Flask, render_template, request,url_for,flash,redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, jsonify, session, g, Response
 from flask_pymongo import PyMongo
-from forms import RegistrationForm,LoginForm
 from flask_bcrypt import Bcrypt
+from backend import (check_auth_login, register_auth_user, verify_usr_eml, string_hash)
+import os
+import secrets
+from itsdangerous import URLSafeTimedSerializer
+from flask_mail import Mail, Message
+import pymongo
+
+
+MONGO_HOST_URL = 'mongodb://localhost:27017/'
+MONGO_DATABASE_NAME = 'Blog'
+
+MAIL_SERVER='smtp@gmail.com' 
+MAIL_PORT=587
+MAIL_USE_TLS=True
+MAIL_USERNAME='dmyhuev376@iemail.one' #it is the email id from which you want to send the mail
+MAIL_PASSWORD='hardik. 203' # it is the password of the email id from which you want to send the mail
+
+
+salt = 'owui4ht3uhtl2thloKSFB'
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://localhost:27017/Blog'
-mongo=PyMongo(app)
-app.config['SECRET_KEY']='5791628bb0b13ce0c676dfde280ba245'
-bcrypt=Bcrypt(app)
+app.secret_key = os.urandom(24)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Blog'
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+app.config['SECURITY_PASSWORD_SALT'] = salt
+app.config['MAIL_SERVER'] = MAIL_SERVER
+app.config['MAIL_PORT'] = MAIL_PORT
+app.config['MAIL_USE_TLS'] = MAIL_USE_TLS
+app.config['MAIL_USERNAME'] = MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
 
-def is_unique(field, value):
-    existing_user = mongo.db.Users.find_one({field: value})
-    already_user=[user for user in existing_user]
-    return already_user
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    remember = request.form.get('remember')
+    if g.user:  # if user is already logged in
+        return render_template('home.html')
+    else:
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            if check_auth_login(request.form['usr_eml'], request.form['usr_pwd']):
+                # it will store the email of the user in the session
+                session['user'] = request.form['usr_eml']
+                # it will store the email of the user in the global variable
+                g.user = session['user']
+                if remember:
+                    session.permanent = True
+                else:
+                    session.permanent = False
+                flash("Logged in Successfully", "success")
+                return redirect(url_for('index'))
+                # return render_template('home.html') # it will redirect to home page if login is successful
+            else:
+                flash("Invalid Credentials", "danger")
+                # it will redirect to login page if login is unsuccessful
+                return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if g.user:
+        return render_template('home.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        if register_auth_user(usr_eml=request.form['usr_eml'], usr_fname=request.form['usr_fname'],
+                              usr_lname=request.form['usr_lname'], usr_pwd=request.form['usr_pwd'],
+                              usr_phone=request.form['usr_phone']):
+            flash("Registered Successfullly", "success")
+            return render_template('login.html')
+        else:
+            flash("Registration Failed", "danger")
+            return render_template('register.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if g.user:
+        g.user = None
+        session.pop('user')
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/verify/<string:key>', methods=['GET'])
+def verify(key):
+    resp = verify_usr_eml(key)
+    return Response(f'<script type="text/javascript"> alert("{resp}") </script>')
 
 
 @app.route('/')
 def index():
-    users=mongo.db.Users.find()
-    user=[user for user in users]
-    posts=mongo.db.Posts.find()
-    post=[post for post in posts]
-    return render_template('home.html',posts=post,user=user)
+    posts = mongo.db.Posts.find()
+    post = [post for post in posts]
+    return render_template('home.html', posts=post)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/register",methods=['GET','POST'])
-def register():
-    form=RegistrationForm()
-    if form.validate_on_submit():
-        if not is_unique('username', form.username.data):
-            flash("Username already exists","danger")
-            print("Username already exists")
-        elif not is_unique('email', form.email.data):
-            flash("Email already exists","danger")
-            print("Email already exists")
-        else:
-            hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-            user={'username':form.username.data,'email':form.email.data,'password':hashed_password,'image_file':'default.jpg',"posts":[]}
-            mongo.db.User.insert_one(user)
-            flash("Your account has been! You are now able to log in","success")
-            return redirect(url_for('login'))
-    return render_template('register.html',title='Register',form=form)
 
-@app.route("/login",methods=['GET','POST'])
-def login():
-    form=LoginForm()
-    if form.validate_on_submit():
-        if form.email.data=='admin@blog.com' and form.password.data=='password':
-            flash("You have been logged in!","success")
-            return redirect(url_for('index'))
+@app.route("/account", methods=['GET', 'POST'])
+def account():
+    if g.user:
+        # check if the updated data is different from the current one or not
+        # it fetches the user from the database with details as a dictionary
+        user = mongo.db.auth_user.find_one({"usr_eml": g.user})
+        if user is None:
+            flash("User not found", "danger")
+            return render_template('login.html')
+        if request.method == 'POST':
+            username = request.form['username']
+            email = request.form['email']
+            if username == user['usr_fname'] or email == user['usr_eml']:
+                mongo.db.auth_user.update_one(
+                    {"usr_eml": g.user}, {"$set": {"usr_fname": username, "usr_eml": email}})
+                flash("Your account has been updated", "success")
+                return redirect(url_for('account'))
+            else:
+                flash("No changes made", "info")
+        return render_template('account.html', title="Account", user=user)
+    else:
+        flash("Please login first", "danger")
+        return render_template('login.html')
+
+
+
+def generate_reset_token(user):
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return s.dumps(user['usr_eml'], salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def verify_reset_token(token):
+    s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = s.loads(token, salt=app.config['SECURITY_PASSWORD_SALT'], max_age=3600)
+        return email
+    except:
+        return None
+
+
+def send_password_reset_email(user):
+    token = generate_reset_token(user)
+    reset_link = request.host_url + f'reset_password/{token}'
+    msg = Message(
+        subject='Password Reset',
+        sender=app.config['MAIL_USERNAME'],
+        recipients=[user['usr_eml']],
+        body=f"Hello {user['usr_fname']},\n\nYou have requested to reset your password. Please click the link below to reset your password:\n\n{reset_link}\n\nIf you didn't request this, please ignore this email.",
+    )
+    mail.send(msg)
+
+
+def generate_password_hash(password):
+    return bcrypt.generate_password_hash(password).decode('utf-8')
+
+
+def check_password_hash(password_hash, password):
+    return bcrypt.check_password_hash(password_hash, password)
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = mongo.db.auth_user.find_one({"usr_eml": email})
+        if user:
+            send_password_reset_email(user)
+            flash('Check your email for the instructions to reset your password', 'info')
         else:
-            flash("Login Unsuccessful. Please check username and password","danger")
-    return render_template('login.html',title='Login',form=form)
+            flash('Email not found', 'warning')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True, port=8000)
---
+++ backend.py
@@ -0,0 +1,128 @@
+# from mailer import send_email_verification_mail
+from flask_mail import Mail, Message
+from flask import request
+import hashlib
+import pymongo
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+
+# Database Configuration parameters
+MONGO_HOST_URL = 'mongodb://localhost:27017/'  # Example: "mongodb+srv://<DB-ID>.mongodb.net" or "mongodb://localhost:27017/"
+MONGO_DATABASE_NAME = 'Blog'
+
+# # Mailjet Configuration
+# MAILJET_API_KEY = '7139ce4e6effcf6a58e90c552c8f0755'
+# MAILJET_API_SECRET = 'bdc3371dbbcd69117fbb542f880bd25a'
+# MAILJET_SENDER_EMAIL = 'garg2003hardik@gmail.com'
+# MAILJET_SENDER_NAME = 'Hardik'
+
+# app.config['MAIL_SERVER'] = 'your-mail-server'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = 'your-email'
+# app.config['MAIL_PASSWORD'] = 'your-password'
+
+
+
+# String Mixer
+salt = 'owui4ht3uhtl2thloKSFB'  # Example: 'owui4ht3uhtl2thloKSFB'
+
+def string_hash(text): 
+    return hashlib.sha256(salt.encode() + text.encode()).hexdigest() + ':' + salt 
+
+
+
+def register_auth_user(usr_fname, usr_lname, usr_eml, usr_pwd, usr_phone):
+    
+    client = pymongo.MongoClient(MONGO_HOST_URL)
+    db = client[MONGO_DATABASE_NAME]
+
+    auth_users_col = db['auth_user'] # Collection for storing user data
+    if auth_users_col.count_documents({'usr_eml': usr_eml}): # Checking if user already exists
+        print(f'User with email {usr_eml} is already created.')
+        return False
+
+    hashed_pwd = string_hash(usr_pwd)
+
+    new_user_data = {'usr_fname': usr_fname, 'usr_lname': usr_lname, 'usr_eml': usr_eml, 'usr_pwd': hashed_pwd,
+                     'usr_phone': usr_phone, 'is_activated': 'False','profile_image':'download.jpeg'}
+    ins = auth_users_col.insert_one(new_user_data)
+
+    # Generating a validation key
+    key, pkey = string_hash(usr_eml).split(':')
+
+    # Setting the validation key in the DB
+    verification_data = {
+        'usr_eml': usr_eml,
+        'key': key
+    }
+    verify_usr_eml_col = db['verify_usr_eml']
+    ins = verify_usr_eml_col.insert_one(verification_data)
+    client.close()
+    link = request.host_url + f'verify/{key}' # host_url = http://  and verify is the route in app.py and key is the key generated above
+    #Send Email verification mail
+    #send_email_verification_mail(usr_eml, usr_fname, link)
+    return ins.acknowledged
+
+
+def check_auth_login(usr_eml, usr_pwd):
+
+    client = pymongo.MongoClient(MONGO_HOST_URL)
+    db = client[MONGO_DATABASE_NAME]
+
+    auth_users_col = db['auth_user']
+    hashed_pwd = string_hash(usr_pwd)
+    search_data = {'usr_eml': usr_eml, 'usr_pwd': hashed_pwd}
+
+    res = auth_users_col.count_documents(search_data)
+    is_activated = auth_users_col.find_one(search_data) # Checking if user is activated or not
+
+    if is_activated is None:
+        return False
+    is_activated = True
+    client.close()
+
+    if res == 1 and is_activated == True:
+        # User verification successful
+        return True
+    else:
+        # User verification failed
+        return False
+
+
+def verify_usr_eml(key):
+    """
+        Function to verify user's email address and activates the account so that user can login to the system.
+    :param key: Email address of the user
+    :return: Success/Failure
+    """
+    search_data = {
+        'key': key,
+    }
+    client = pymongo.MongoClient(MONGO_HOST_URL)
+    db = client[MONGO_DATABASE_NAME]
+
+    verify_usr_eml_col = db['verify_usr_eml']
+    auth_user_col = db['auth_user']
+    res = verify_usr_eml_col.count_documents(search_data)
+    if res == 1:
+        usr_eml = verify_usr_eml_col.find_one(search_data, {'_id': 0, 'usr_eml': 1}).get('usr_eml')
+        user_search_data = {
+            'usr_eml': usr_eml
+        }
+        new_data = {
+            'is_activated': 'True'
+        }
+        auth_user_col.update_one(user_search_data, {"$set": new_data})
+
+        verify_usr_eml_col.delete_one(search_data)
+        return f'User verification for {usr_eml} successful!!'
+    else:
+        return f'User verification failed!!'
+
+
+    
+
